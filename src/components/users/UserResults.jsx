b90f2f706782595a0912840b078e1187df0d1a36
Peter Murutu <murutupeter@gmail.com>
2022-05-01 22:20:28 +0300

    search component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,10 @@
 import Spinners from "../layout/Spinners";
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const { users, loading, fetchUsers } = useContext(GithubContext);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { users, loading } = useContext(GithubContext);
 
   if (!loading) {
     return (
